f23e901d3f4764dca416a89cdb6e694ea902eaaa
Osman-Lutufia <110116081+Osman-Lutufia@users.noreply.github.com>
2022-08-17 11:49:31 -0700

    created a react form

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import React from 'react'
+import ProfileCard from './components/ProfileCard'
+import ProfileForm from './components/ProfileForm'
+
+function App() {
+  return (
+    <div>
+      
+
+      <ProfileCard />
+      <ProfileForm/>
+      </div>
+  )
+}
+
+export default App
